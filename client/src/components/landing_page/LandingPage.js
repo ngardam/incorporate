c76e066a9cd50650a4d4ca8e95a71aee714759d9
Nick Gardam <Nick@jessicas-air.lan>
2022-10-30 22:25:20 -0400

    basic landing page styling, browser router dom installed

---
+++ client/src/components/landing_page/LandingPage.js
@@ -0,0 +1,14 @@
+function LandingPage(){
+    return(
+        <div>
+
+            <h1>
+            Incorporate.
+          </h1>
+          <button className="btn">Sign In</button>
+          <button className="btn">Sign Up</button>
+        </div>
+    );
+    }
+
+export default LandingPage
